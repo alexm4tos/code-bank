60a77c07f325e22c46d37f00293e9d09969325de
Alex Matos <alex.matos.freitas@gmail.com>
2022-08-18 20:49:01 -0300

    store-frontend: create next app with tailwindcss

---
+++ store-frontend/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { NextPage } from 'next/types';
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>Store frontend</title>
+			</Head>
+
+			<h1>Store frontend</h1>
+		</>
+	);
+};
+
+export default Home;
